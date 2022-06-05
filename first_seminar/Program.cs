f769c551bf27821cba921fdbd32def74be955b55
Shohjahon Abdumuminov <rakhmatovichsh@gmail.com>
2022-06-05 12:09:09 +0300

    возведение в квадрат

--- first_seminar/Program.cs
+++ first_seminar/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+
+Console.WriteLine(a * a);
